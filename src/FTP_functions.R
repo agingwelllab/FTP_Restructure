f7cd9b253beac6a07c160dcf1b25d7f7902dde59
sspill <ssp160230@utdallas.edu>
2021-09-10 10:11:44 -0500

    reformatted from tidyverse style guide

--- src/FTP_functions.R
+++ src/FTP_functions.R
@@ -1,16 +1,20 @@
+# create function to clean up raw data frame
 fclean <- function(fraw_df) {
+  
   # remove unnecessary column for "complete" form status
   fwide <- select(fraw_df, -ftp_complete)
+  
   # reverse score q8, q9, q10
   f_rscored <- fwide %>%
     transform(ftp_8r = 7 - ftp_8r, ftp_9r = 7 - ftp_9r, ftp_10r = 7 - ftp_10r)
+  
   # create a column and calculate mean scores for each participant
   f_clean <- f_rscored %>%
     mutate(ftp_mean = rowMeans(f_rscored[-1]))
 }
 
+# create function to return separate df with record_id and mean scores
 fmean <- function(f_clean) {
-  # returns df with 2 columns: record_id and ftp_mean scores
   f_mean <- f_clean %>%
     transmute(record_id, ftp_mean)
 }
