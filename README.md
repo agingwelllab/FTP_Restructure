ce8d44a48793c4e426c2ca6cfb98594c85b28a12
sspill <ssp160230@utdallas.edu>
2021-09-17 16:23:09 -0500

    changed functions code to account for 3 components of FTP

--- README.md
+++ README.md
@@ -3,11 +3,16 @@ Repo contains code to restructure data for the Future Time Perspective (FTP) mea
 
 ## Functions
 * fclean(*df*)
+  * restructures data frame from wide format to long format
   * input: raw data frame imported from CSV (*fraw_df* in functions script)
-  * output: data frame with reverse scoring for questions 8-10 and mean scores for each participant (*f_clean* in functions script)
-    * 12 columns: record_id, 1 column for each variable/question formatted as ftp_(question number), and ftp_mean
-    * variables for questions 8-10 are labeled ftp_8r, ftp_9r, and ftp_10r to indicate reverse scoring for these responses
+    * variable names as columns, separate rows for each participant
+  * output: data frame with reverse scoring for constraint questions (*ftotal_clean* in functions script)
+    * 4 columns: record_id, component, q_number, and response
+    * each record_id has 10 rows (one for each question)
 * fmean(*df*)
+  * calculates average score per component for each record_id (3-4 questions per component)
+    * perceived future time opportunity (opp), perceived future time extension (ext), and perceived future time constraint (cons)
   * input: clean data frame from fclean(*df*)'s output
-  * output: data frame with mean scores for each record_id (*f_mean* in functions script)
-    * 2 columns: record_id and ftp_mean
+  * output: data frame with mean scores for each record_id (*ftotal_mean* in functions script)
+    * 4 columns: record_id, cons, ext, and opp
+    * each record_id has 1 row
--- src/FTP_functions.R
+++ src/FTP_functions.R
@@ -4,17 +4,43 @@ fclean <- function(fraw_df) {
   # remove unnecessary column for "complete" form status
   fwide <- select(fraw_df, -ftp_complete)
   
-  # reverse score q8, q9, q10
+  # reverse score constraint questions
   f_rscored <- fwide %>%
-    transform(ftp_8r = 8 - ftp_8r, ftp_9r = 8 - ftp_9r, ftp_10r = 8 - ftp_10r)
+    transform(cons_1 = 8 - cons_1, cons_2 = 8 - cons_2, cons_3 = 8 - cons_3)
   
-  # create a column and calculate mean scores for each participant
-  f_clean <- f_rscored %>%
-    mutate(ftp_mean = rowMeans(f_rscored[-1]))
+  # restructure df to change from wide to long format
+  flong <- f_rscored %>%
+    gather(variables, response, opp_1:cons_3, factor_key = TRUE)
+  
+  #separate variables column into ftp_component and q_number
+  ftotal <- flong %>%
+    separate(variables, c('component', 'q_number'), '_')
+  
+  # change 'q_number' column from chr to num
+  ftotal$q_number <- as.numeric(ftotal$q_number)
+  
+  # sort by record_id and component
+  ftotal_clean <- ftotal %>%
+    arrange(record_id, component)
 }
 
-# function to return separate df with record_id and mean scores
-fmean <- function(f_clean) {
-  f_mean <- f_clean %>%
-    transmute(record_id, ftp_mean)
+# function to create new df with mean component scores from clean df
+fmean <- function(ftotal_clean) {
+  
+  # create df with mean component scores for each record_id
+  ftotal_mean_raw <- aggregate(
+    response ~ record_id + component, 
+    data = ftotal_clean, 
+    FUN = mean
+  )
+  
+  # round mean scores to 2 decimals
+  ftotal_mean_raw$response <- round(ftotal_mean_raw$response, digits = 2)
+  
+  # pivot df to wide format to get components as columns
+  ftotal_mean <- pivot_wider(
+    data = ftotal_mean_raw, 
+    names_from = component, 
+    values_from = response
+  )
 }
