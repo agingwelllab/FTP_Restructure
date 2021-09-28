b3301a43dcb3918a3c8782ad43762fad4e8255d4
sspill <ssp160230@utdallas.edu>
2021-09-28 14:21:11 -0500

    modified based on pull request for other repos

--- clean_FTP_data.R
+++ clean_FTP_data.R
@@ -3,29 +3,18 @@
 
 # load required packages
 library(tidyverse)
+library(here)
 
 # load source functions
-source('/Users/ssp160230/github_directory/FTP_Restructure/src/FTP_functions.R')
+source(here::here('src', 'FTP_functions.R'))
 
-# set hard-coded variables 
-ftp_raw <- read_csv('/Users/ssp160230/github_directory/FTP_Restructure/data/FTP_data.csv')
+# set hard-coded variables
+
+# load data
+ftp_raw <- read_csv(here::here('data', 'FTP_data.csv'))
 
 # clean and reformat raw data frame
 ftp_clean <- fclean(ftp_raw)
 
 # calculate mean scores for each participant
 ftp_mean <- fmean(ftp_clean)
-
-# write csv for clean df
-write.csv(
-  ftp_clean, 
-  "/Users/ssp160230/github_directory/FTP_Restructure/output/FTP_clean.csv", 
-  row.names = FALSE
-)
-
-# write csv for mean scores df
-write.csv(
-  ftp_mean, 
-  "/Users/ssp160230/github_directory/FTP_Restructure/output/FTP_mean.csv", 
-  row.names = FALSE
-)
